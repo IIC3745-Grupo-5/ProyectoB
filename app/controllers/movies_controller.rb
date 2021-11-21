43adcb9a09d3f0f9656c7cf8cc622abc0f7c0f80
Diego Solari <dasolari@uc.cl>
2021-11-21 03:29:59 -0300

    feat: added movie show

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,6 @@
 class MoviesController < ApplicationController
-  before_action :set_movie, only: %i[ show edit update destroy ]
+  before_action :set_movie, only: %i[ show ]
+  before_action :set_schedules, only: %i[ show ]
 
   # GET /movies or /movies.json
   def index
@@ -8,58 +9,24 @@ class MoviesController < ApplicationController
 
   # GET /movies/1 or /movies/1.json
   def show
-  end
-
-  # GET /movies/new
-  def new
-    @movie = Movie.new
-  end
-
-  # GET /movies/1/edit
-  def edit
-  end
-
-  # POST /movies or /movies.json
-  def create
-    @movie = Movie.new(movie_params)
-
-    respond_to do |format|
-      if @movie.save
-        format.html { redirect_to @movie, notice: "Movie was successfully created." }
-        format.json { render :show, status: :created, location: @movie }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @movie.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /movies/1 or /movies/1.json
-  def update
-    respond_to do |format|
-      if @movie.update(movie_params)
-        format.html { redirect_to @movie, notice: "Movie was successfully updated." }
-        format.json { render :show, status: :ok, location: @movie }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @movie.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /movies/1 or /movies/1.json
-  def destroy
-    @movie.destroy
-    respond_to do |format|
-      format.html { redirect_to movies_url, notice: "Movie was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    @morning_schedule = @schedules.find { |schedule| schedule.time == "Morning" }
+    @afternoon_schedule = @schedules.find { |schedule| schedule.time == "Afternoon" }
+    @evening_schedule = @schedules.find { |schedule| schedule.time == "Evening" }
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_movie
       @movie = Movie.find(params[:id])
+      @cinema = set_cinema(@movie.cinema_id)
+    end
+
+    def set_cinema(cinema_id)
+      cinema = Cinema.find(cinema_id)
+    end
+
+    def set_schedules
+      @schedules = Schedule.where(movie_id: params[:id])
     end
 
     # Only allow a list of trusted parameters through.
